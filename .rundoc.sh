3804f6ec674282cfdc0d247ca3b4beb6e6313dca
Dennis Ploetner <re@lloc.de>
2012-11-30 11:50:53 +0100

    Delete old doc

--- .rundoc.sh
+++ .rundoc.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 
-phpdoc run -t . -d ../master/ --ignore="index.php,tests/*" --title="Multisite Language Switcher"
+phpdoc run -t . -d ../master/ --ignore="index.php,tests/*" --title="Multisite Language Switcher" --template zend
