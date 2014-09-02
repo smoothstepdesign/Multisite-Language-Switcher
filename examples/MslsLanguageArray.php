caf9afa26f6047ed8e0bd79ea398b10fadd20db7
lloc <re@lloc.de>
2014-09-02 10:28:19 +0200

    Examples

---
+++ examples/MslsLanguageArray.php
@@ -0,0 +1,11 @@
+<?php
+
+$arr = array(
+	'it'    => 1,
+	'de_DE' => 2,
+);
+$obj = new MslsLanguageArray( $arr );
+$val = $obj->get_val( 'it' );         // 1 == $val
+$val = $obj->get_val( 'fr_FR' );      // 0 == $val
+$val = $obj->get_arr();               // array( 'it' => 1, 'de_DE' => 2 ) == $val
+$val = $obj->get_arr( 'de_DE' );      // array( 'it' => 1 ) == $val
