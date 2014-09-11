22fc093a9dd555b6508c40e16139eaf318904f9a
lloc <re@lloc.de>
2014-09-11 12:31:13 +0200

    API Documentation refreshed

--- examples/MslsJson.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$obj = new MslsJson;
-$obj->add( null, 'Test 3' )
-	->add( '2', 'Test 2' )
-	->add( 1, 'Test 1' );
-
-// Output: [{"value":1,"label":"Test 1"},{"value":2,"label":"Test 2"},{"value":0,"label":"Test 3"}]
-echo $obj;
